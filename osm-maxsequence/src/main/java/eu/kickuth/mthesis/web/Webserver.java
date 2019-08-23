c55e92eadf3638f9de69e79f7471fd99f9f146db
Marvin Kickuth <marvin.kickuth@uni-konstanz.de>
2019-08-23 16:45:33 +0200

    wip sidebar functionality

--- osm-maxsequence/src/main/java/eu/kickuth/mthesis/web/Webserver.java
+++ osm-maxsequence/src/main/java/eu/kickuth/mthesis/web/Webserver.java
@@ -5,6 +5,8 @@ import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+import spark.Request;
+import spark.Response;
 
 import java.io.StringWriter;
 
@@ -12,17 +14,29 @@ import static spark.Spark.*;
 
 public class Webserver {
 
+    private static final VelocityEngine ve = new VelocityEngine();
+    private static String geoJSON;
+    private static String poiJSON;
+
     public static void start(String geoJSON, String poiJSON) {
+        // TODO refactor
+        Webserver.geoJSON = geoJSON;
+        Webserver.poiJSON = poiJSON;
 //        staticFiles.location("/web");  // Static files
 //        staticFiles.expireTime(600); // cache static files for ten minutes
 
-        // get and initialize an engine
-        VelocityEngine ve = new VelocityEngine();
+        // initialize engine
         ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
         ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
         ve.init();
 
-        // load html template
+        // return the web page
+        get("/", Webserver::renderMap);
+        post("/", Webserver::renderMap);
+    }
+
+    private static String renderMap(Request req, Response res) {
+        // load map html template
         Template htmlTemplate = ve.getTemplate( "web/map.vm" );
 
         // create context to add data
@@ -35,7 +49,6 @@ public class Webserver {
         StringWriter writer = new StringWriter();
         htmlTemplate.merge( htmlContext, writer );
 
-        // return the web page
-        get("/", (req, res) -> writer.toString());
+        return writer.toString();
     }
 }
